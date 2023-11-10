dde8a8226ec8c9b60a2f2715814834a0b9015da3
lotodee <olamidotun.loto@decagon.dev>
2023-11-10 11:43:26 +0100

    m

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ import Slide from '../../components/slide/Slide';
 import {calender,news,events, projects, gigs } from '../../data';
 import CatCard from '../../components/catCard/CatCard';
 
-import ProjectCard from '../../components/projectCard/projectCard';
+import ProjectCard from '../../components/ProjectCard/projectCard';
 import Calender from '../../components/calender/Calender';
 
 import "./Home.scss"
